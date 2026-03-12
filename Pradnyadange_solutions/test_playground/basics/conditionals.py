ba5cb87d675378843fa2c2cb72ed1851d304e81e
Pradnyadange <pradnyadange07@gmail.com>
2026-03-12 09:20:13 +0530

    Update conditionals.py

--- Pradnyadange_solutions/test_playground/basics/conditionals.py
+++ Pradnyadange_solutions/test_playground/basics/conditionals.py
@@ -1,40 +1,49 @@
 # correct if else ladder to check if person is underage, normal citizen or senior citizen
 # [0,18) -> underage, [18,60) normal age, [60,inf) senior citizen
 # bonus, can you reduce ladder to a one liner?
-age = input("Enter age") # ahh yes age is str , definitely
+age = int(input("Enter age")) # ahh yes age is str , definitely
 
-if age <= 0:
-    print("Lil bro")
-elif age > 100:
-    print("Pay up taxes, person")
+if age < 18:
+    print("underage")
+elif age > 60:
+    print("normal age")
 else:
-    print("U still good, unc?")
-
+    print("senior citizen")
+#One liner code
+print("underage" if age < 18 else "normal citizen" if age < 60 else "senior citizen")
 
 # complete the match
 
-day = input("Enter the day number") # dont forget to typecast to int
+day = int(input("Enter the day number")) # dont forget to typecast to int
 
-print("Today is: ") # how can you avoid printing newline here?
+print("Today is: , end=") # how can you avoid printing newline here?
 
 match day:
     case 1:
         print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturaday")
+    case 8:
+        print("Sunday")
     # fill in the rest
-    case _:
+    case 9:
         print("Funday !") 
 
 # implement try catch
 
 try:
+    a = 0 
+    b = 1
     print(1/0)
-except IndentationError: # ahh fix the syntax, also when u don't know the error what will u use?
+except IndentationError as e: # ahh fix the syntax, also when u don't know the error what will u use?
     print("what u tryna do bro")
 finally:
     print("So u done?")
-
-
-
-
-
-
